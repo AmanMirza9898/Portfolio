ccb6240c72f7001c733260303447d9151e2b3c6a
Aman Mirza <155296756+AmanMirza9898@users.noreply.github.com>
2025-09-25 22:25:51 +0530

    Update import utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from 'clsx'
-
+import { twMerge } from 'tailwind-merge'
 // Safe way - koi import error nahi ayega
-const { twMerge } = require('tailwind-merge')
+// const { twMerge } = require('tailwind-merge')
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
