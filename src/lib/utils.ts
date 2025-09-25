e25b49758183dc457bfbefdce92b499a9e2dd454
AmanMirza9898 <aman9811269898@gmail.com>
2025-09-25 20:20:11 +0530

    fix: resolve build errors by using require import in utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,6 @@
 import { clsx, type ClassValue } from 'clsx'
+
+// Safe way - koi import error nahi ayega
 const { twMerge } = require('tailwind-merge')
 
 export function cn(...inputs: ClassValue[]) {
